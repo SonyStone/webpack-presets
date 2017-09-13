affd4078536e669e858c7e926acea0d970ac1df0
Driver <iliaverpa@gmail.com>
2017-09-13 13:53:06 +0300

    devServer beautify

--- config/webpack/devserver.js
+++ config/webpack/devserver.js
@@ -2,24 +2,22 @@ const {
   HotModuleReplacementPlugin,
 } = require('webpack');
 
-
-/**
- * ```
- * npm install webpack-dev-server --save-dev
- * ```
- * 
- * webpack-dev-server
- * [Git]{@link https://github.com/webpack/webpack-dev-server }
- * [Documentation]{@link https://webpack.js.org/configuration/dev-server/#devserver }
- * 
- *
- */
-
 module.exports = function() {
   return {
+    /**
+     * ```
+     * npm install webpack-dev-server --save-dev
+     * ```
+     * 
+     * webpack-dev-server
+     * [Git]{@link https://github.com/webpack/webpack-dev-server }
+     * [Documentation]{@link https://webpack.js.org/configuration/dev-server/#devserver }
+     * 
+     *
+     */
     devServer: {
-      // stats: 'errors-only',
       /** [doc]{@link https://webpack.js.org/configuration/dev-server/#devserver-disablehostcheck} */
+      // stats: 'errors-only',
       disableHostCheck: true,
       hot: true,
       historyApiFallback: true,
